8a7b7658749f8449ae113d0dbd9729a5b54225f6
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-05-07 18:52:20 -0300

    Test

--- src/BottenderApp.js
+++ src/BottenderApp.js
@@ -10,13 +10,28 @@ export default async function App(context) {
 	await context.sendText(`Você mandou: ${JSON.stringify(context.event.message)}`);
 
 	const { message } = context.event;
-	if (message.type === 'button') {
-		const { value } = message;
-		if (value === 'teste') {
+
+
+	switch (message.type) {
+	case 'button':
+		await context.setState({ lastButton: message.value });
+		if (context.state.lastButton === 'teste') {
 			await context.setState({ dialog: 'teste' });
 		} else {
-			await context.setState({ dialog: value });
+			await context.setState({ dialog: context.state.lastButton });
 		}
+		break;
+
+	case 'text':
+		await context.setState({ whatWasTyped: message.value });
+		if (context.state.dialog === 'teste') {
+			await context.setState({ dialog: 'teste' });
+		} else {
+			console.log('context.state.whatWasTyped', context.state.whatWasTyped);
+		}
+		break;
+	default:
+		break;
 	}
 
 
@@ -24,6 +39,7 @@ export default async function App(context) {
 	case 'greetings':
 		await context.sendText(flow.greetings.text1.replace('<USERNAME>', 'cidadão'));
 		await attach.sendMsgFromAssistente(context, 'greetings', [flow.greetings.text2]);
+		await context.sendTextFreeText('Qual é a sua dúvida?');
 		// await dialogs.sendMainMenu(context, flow.mainMenu.firstTime);
 		break;
 
